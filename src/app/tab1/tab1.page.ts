2e10d7462be90594c05be995856a40329a32151b
Alberto Marques <prof.amarques@gmail.com>
2021-05-28 16:34:38 -0300

    Adiconado array de filmes e a interface de filmes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from './../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,29 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Videos';
+  listaVideos: IFilme[] =[
+    {
+      nome: 'Setembro em Shiraz (2015)',
+      lancamento: '24/06/2016',
+      duracao: '1h 50m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/l7BZsQDslgmltoM7Mxv8ZOBuCUY.jpg',
+      generos: ['Drama', 'Thriller']
+    },
+
+    {
+      nome: 'Livrai-nos do Mal (2014)',
+      lancamento: '10/09/2014',
+      duracao: '1h 58m',
+      classificacao: 62,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/t1PBqWmRrfLszd7gsO4AQjZwrvQ.jpg',
+      generos: ['Thriller', 'Crime', 'Terror']
+    },
+
+  ];
+
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
