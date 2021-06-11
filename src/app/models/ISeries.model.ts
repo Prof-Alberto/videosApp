440c5e6c81f3d377632ee0f2f071b208b4633a20
Alberto Marques <prof.amarques@gmail.com>
2021-06-11 16:30:50 -0300

    Adicionado array de séries e a interface de séries.

---
+++ src/app/models/ISeries.model.ts
@@ -0,0 +1,9 @@
+export interface ISeries {
+  nome: string;
+  lancamento: string;
+  duracao: string;
+  classificacao: number;
+  censura: string;
+  cartaz: string;
+  generos: string[];
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
+import { ISeries } from './../models/ISeries.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -8,7 +10,56 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab2Page {
 
-  constructor(public alertController: AlertController) {}
+  titulo = 'Séries TV';
+  listaSeries: ISeries[] = [
+    {
+      nome:'Evil: Contatos Sobrenaturais',
+      lancamento:'2019',
+      duracao: 'Episódios de 45mim',
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qlHzOQOkBTfdMdMd5rQNNli2YLN.jpg',
+      censura: '16 anos',
+      classificacao: 79,
+      generos: ['Drama', 'Mistério']
+    },
+    {
+      nome:'Lei & Ordem: Crimes Premeditados',
+      lancamento:'2001',
+      duracao: 'Episódios de 60mim',
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xMxeq0IMQsuyJ4itzUS0WAh9Ukh.jpg',
+      censura: '14 anos',
+      classificacao: 76,
+      generos: ['Drama','Crime']
+    },
+    {
+      nome:'Lei & Ordem: Unidade de Vítimas Especiais',
+      lancamento:'1999',
+      duracao: 'Episódios de 43mim',
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/asVCgrKySiGL7TozzcU4L1l1qQM.jpg',
+      censura: '16 anos',
+      classificacao: 78,
+      generos: ['Drama','Crime']
+    },
+    {
+      nome:'The Good Doctor: O Bom Doutor',
+      lancamento:'2017',
+      duracao: 'Episódios de 43mim',
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ucMdbTpOhV75R0NtxqHEg4hirNl.jpg',
+      censura: '12 anos',
+      classificacao: 86,
+      generos: ['Drama']
+    },
+    {
+      nome:'Private Eyes',
+      lancamento:'2016',
+      duracao: 'Episódios de 43mim',
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/HsNFA4ZbjZkAnPtBYJAIORFhPP.jpg',
+      censura: '12 anos',
+      classificacao: 73,
+      generos: ['Drama','Crime']
+    }
+  ];
+
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
 
   async exiberAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -25,13 +76,19 @@ export class Tab2Page {
         }, {
           text: 'SIM',
           handler: () => {
-            console.log('Confirm Okay');
+            this.apresentarToast();
           }
         }
       ]
     });
-
     await alert.present();
   }
-
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Série adiconada as favoritas!',
+      duration: 3000,
+      color: 'medium'
+    });
+    toast.present();
+  }
 }
